d3b2765e4722fece9bafa8b62298251ca9c4b746
Criste Phillips <criste.phillips@yahoo.com>
2022-03-29 16:38:29 -0400

    get, put, post, delete methods initial

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,107 +1,85 @@
 const { Thought } = require("../models");
 
 const thoughtController = {
-// GET to get all thoughts
-
-// GET to get a single thought by its _id
-
-// POST to create a new thought (push the created thought's _id to the associated user's thoughts array field)
-    // example data
-
-    // {
-    //   "thoughtText": "Here's a cool thought...",
-    //   "username": "lernantino",
-    //   "userId": "5edff358a0fcb779aa7b118b"
-    // }
-
-
-// PUT to update a thought by its _id
-
-// DELTE to remove a thought by its _id
-
-
-
-// /api/thoughts/:thoughtId/reactions
-
-// POST to create a reaction stored in a single thought's reactions array field
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value 
-
-  // get all thought data
-  getAllSocial(req, res) {
-    Social.find({})
-      .populate({
-        path: "comments",
-        select: "-__v",
-      })
-      .select("-__v")
-      .sort({ _id: -1 })
-      .then((dbSocialData) => res.json(dbSocialData))
+  // GET to get all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
   },
-
-  // get one component social data by id
-  getSocialById({ params }, res) {
-    Social.findOne({ _id: params.id })
-      .populate({
-        path: "comments",
-        select: "-__v",
-      })
-      .select("-__v")
-      .then((dbSocialData) => {
-        // If no social data is found, send 404
-        if (!dbSocialData) {
-          res
-            .status(404)
-            .json({ message: "No social data found with this id!" });
+  // GET to get a single thought by its _id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        // If no thought is found, send 404
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
           return;
         }
-        res.json(dbSocialData);
+        res.json(dbThoughtData);
       })
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
   },
-  // createSocial
-  createSocial({ body }, res) {
-    Social.create(body)
-      .then((dbPizzaData) => res.json(dbSocialData))
+
+  // POST to create a new thought (push the created thought's _id to the associated user's thoughts array field); createThought; destructure the body out of express.js req object
+            // example data
+
+            // {
+            //   "thoughtText": "Here's a cool thought...",
+            //   "username": "lernantino",
+            //   "userId": "5edff358a0fcb779aa7b118b"
+            // }
+
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => res.json(err));
   },
 
-  // update social data by id; runValidators included to validate all new info
-  updateSocial({ params, body }, res) {
-    Social.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-      .then((dbSocialData) => {
-        if (!dbSocialData) {
+  // PUT to update a thought by its _id; runValidators included to validate all new info
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
           res
             .status(404)
-            .json({ message: "No social data found with this id!" });
+            .json({ message: "No thought data found with this id!" });
           return;
         }
-        res.json(dbSocialData);
+        res.json(dbThoughtData);
       })
       .catch((err) => res.status(400).json(err));
   },
 
-  // delete social data
-  deleteSocial({ params }, res) {
-    Social.findOneAndDelete({ _id: params.id })
-      .then((dbSocialData) => {
-        if (!dbSocialData) {
+  // DELETE to remove a thought by its _id
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
           res
             .status(404)
-            .json({ message: "No social data found with this id!" });
+            .json({ message: "No thought data found with this id!" });
           return;
         }
-        res.json(dbSocialData);
+        res.json(dbThoughtData);
       })
       .catch((err) => res.status(400).json(err));
   },
 };
 
-module.exports = socialController;
+// /api/thoughts/:thoughtId/reactions
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,26 +3,74 @@ const { User, Thought } = require("../models");
 
 // create userController object
 const userController = {
+  // GET all users
+  getAllUser(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
+  // GET a single user by _id & populated thought & friend data
+  // is this right (??)
+  getUserById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // POST a new user
+  // example:
+  // {
+  //   "username": "lernantino",
+  //   "email": "lernantino@gmail.com"
+  // }
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 
-// GET all users
+  // PUT to update a user by its _id; runValidators included to validate all new info
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User data found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 
-// GET a single user by _id & populated thought & friend data
-
-// POST a new user
-    // example:
-    // {
-    //   "username": "lernantino",
-    //   "email": "lernantino@gmail.com"
-    // }
-
-
-// PUT to update a user by its _id
-
-// DELTE to remove user by its _id
-
-
-
+  // DELETE to remove a user by its _id
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User data found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
 
 // /api/users/:userId/friends/:friendId
 
@@ -30,94 +78,80 @@ const userController = {
 
 // DELETE to remove a friend from a user's friend list
 
+// // add comment to social data
+// addComment({ params, body }, res) {
+//   console.log(body);
+//   Comment.create(body)
+//     .then(({ _id }) => {
+//       return Social.findOneAndUpdate(
+//         { _id: params.socialId },
+//         { $push: { comments: _id } },
+//         { new: true }
+//       );
+//     })
+//     .then((dbSocialData) => {
+//       if (!dbSocialData) {
+//         res
+//           .status(404)
+//           .json({ message: "No social data found with this id!" });
+//         return;
+//       }
+//       res.json(dbSocialData);
+//     })
+//     .catch((err) => res.json(err));
+// },
+// // add reply
+// addReply({ params, body }, res) {
+//   Comment.findOneAndUpdate(
+//     { _id: params.commentId },
+//     { $push: { replies: body } },
+//     { new: true, runValidators: true }
+//   )
+//     .then((dbSocialData) => {
+//       if (!dbSocialData) {
+//         res
+//           .status(404)
+//           .json({ message: "No social data found with this id!" });
+//         return;
+//       }
+//       res.json(dbSocialData);
+//     })
+//     .catch((err) => res.json(err));
+// },
 
-
-
-
-
-
-
-
-
-
-
-
-
-  // // add comment to social data
-  // addComment({ params, body }, res) {
-  //   console.log(body);
-  //   Comment.create(body)
-  //     .then(({ _id }) => {
-  //       return Social.findOneAndUpdate(
-  //         { _id: params.socialId },
-  //         { $push: { comments: _id } },
-  //         { new: true }
-  //       );
-  //     })
-  //     .then((dbSocialData) => {
-  //       if (!dbSocialData) {
-  //         res
-  //           .status(404)
-  //           .json({ message: "No social data found with this id!" });
-  //         return;
-  //       }
-  //       res.json(dbSocialData);
-  //     })
-  //     .catch((err) => res.json(err));
-  // },
-  // // add reply
-  // addReply({ params, body }, res) {
-  //   Comment.findOneAndUpdate(
-  //     { _id: params.commentId },
-  //     { $push: { replies: body } },
-  //     { new: true, runValidators: true }
-  //   )
-  //     .then((dbSocialData) => {
-  //       if (!dbSocialData) {
-  //         res
-  //           .status(404)
-  //           .json({ message: "No social data found with this id!" });
-  //         return;
-  //       }
-  //       res.json(dbSocialData);
-  //     })
-  //     .catch((err) => res.json(err));
-  // },
-
-  // // remove reply
-  // removeReply({ params }, res) {
-  //   Comment.findOneAndUpdate(
-  //     { _id: params.commentId },
-  //     { $pull: { replies: { replyId: params.replyId } } },
-  //     { new: true }
-  //   )
-  //     .then((dbdbSocialData) => res.json(dbSocialData))
-  //     .catch((err) => res.json(err));
-  // },
-  // // remove comment; delete document while returning data
-  // removeComment({ params }, res) {
-  //   Comment.findOneAndDelete({ _id: params.commentId })
-  //     .then((deletedComment) => {
-  //       if (!deletedComment) {
-  //         return res.status(404).json({ message: "No comment with this id!" });
-  //       }
-  //       return Pizza.findOneAndUpdate(
-  //         { _id: params.socialId },
-  //         { $pull: { comments: params.commentId } },
-  //         { new: true }
-  //       );
-  //     })
-  //     .then((dbSocialData) => {
-  //       if (!dbSocialData) {
-  //         res
-  //           .status(404)
-  //           .json({ message: "No social data found with this id!" });
-  //         return;
-  //       }
-  //       res.json(dbSocialData);
-  //     })
-  //     .catch((err) => res.json(err));
-  // },
-};
+// // remove reply
+// removeReply({ params }, res) {
+//   Comment.findOneAndUpdate(
+//     { _id: params.commentId },
+//     { $pull: { replies: { replyId: params.replyId } } },
+//     { new: true }
+//   )
+//     .then((dbdbSocialData) => res.json(dbSocialData))
+//     .catch((err) => res.json(err));
+// },
+// // remove comment; delete document while returning data
+// removeComment({ params }, res) {
+//   Comment.findOneAndDelete({ _id: params.commentId })
+//     .then((deletedComment) => {
+//       if (!deletedComment) {
+//         return res.status(404).json({ message: "No comment with this id!" });
+//       }
+//       return Pizza.findOneAndUpdate(
+//         { _id: params.socialId },
+//         { $pull: { comments: params.commentId } },
+//         { new: true }
+//       );
+//     })
+//     .then((dbSocialData) => {
+//       if (!dbSocialData) {
+//         res
+//           .status(404)
+//           .json({ message: "No social data found with this id!" });
+//         return;
+//       }
+//       res.json(dbSocialData);
+//     })
+//     .catch((err) => res.json(err));
+// },
 
 module.exports = commentController;
